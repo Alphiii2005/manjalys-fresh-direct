161f0137f6dcb11b0069e45542c72b2ed01993e1
Alphy <18alphinnn@gmail.com>
2026-07-22 17:06:51 +0100

    Add product image uploads

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -3,6 +3,11 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(
+        max_length=None,
+        use_url=True
+    )
+
     class Meta:
         model = Product
         fields = "__all__"
