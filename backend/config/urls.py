58b5deefba6d39c93c9167d5d372a40988f511a9
Alphy <18alphinnn@gmail.com>
2026-08-27 18:41:02 +0100

    view site problem solved

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_url = "http://localhost:3000/"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
